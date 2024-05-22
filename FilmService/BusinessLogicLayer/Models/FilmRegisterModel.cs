2278736c947054a9cb3ff4f876959efa53186325
Rokotilli <valentin.romanyuk.2004@gmail.com>
2024-05-22 14:41:49 +0300

    Забув Quality додати в модель реєстрації фільму(

--- FilmService/BusinessLogicLayer/Models/FilmRegisterModel.cs
+++ FilmService/BusinessLogicLayer/Models/FilmRegisterModel.cs
@@ -15,6 +15,7 @@ namespace BusinessLogicLayer.Models
         [RegularExpression(@"\d+[.](0[0-9]|1[0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]",
             ErrorMessage = "Duration must be in the format 'd.hh:mm:ss'")]
         public string Duration { get; set; }
+        public string Quality { get; set; }
         public string Country { get; set; }
         public int AgeRestriction { get; set; }
         public DateTime DateOfPublish { get; set; }
