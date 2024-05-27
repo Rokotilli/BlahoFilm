9ca86cfd8d794072148eafedeb3a1fcff153b5ea
Rokotilli <valentin.romanyuk.2004@gmail.com>
2024-05-27 13:31:17 +0300

    Прибрав лишнє

--- SeriesService/SeriesServiceAPI/Program.cs
+++ SeriesService/SeriesServiceAPI/Program.cs
@@ -1,13 +1,4 @@
-using DataAccessLayer.Context;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<SeriesServiceDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SeriesServiceSqlServer"));
-});
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
