f2c3227e7e0b893d166088c8236bbe98987408df
DemchenkoVladyslav <demchenkovlad80@gmail.com>
2024-06-07 02:03:10 +0300

    UPD for one AnimePart EndPoint

--- AnimeService/AnimeServiceAPI/Controllers/AnimePartController.cs
+++ AnimeService/AnimeServiceAPI/Controllers/AnimePartController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLayer.Context;
 using DataAccessLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace AnimeServiceAPI.Controllers
 {
     [ApiController]
@@ -15,7 +16,7 @@ namespace AnimeServiceAPI.Controllers
         [HttpGet("getbyanimeid")]
         public async Task<IActionResult> GetAnimePartsByAnimeId([FromQuery] int animeId)
         {
-            var model = _dbContext.AnimeParts.Where(ap => ap.AnimeId == animeId)
+            var model = _dbContext.AnimeParts.Include(ap=>ap.Anime).Where(ap => ap.AnimeId == animeId)
                 .Select(a => new
                 {
                     Id = a.Id,
@@ -39,20 +40,29 @@ namespace AnimeServiceAPI.Controllers
         [HttpGet("getbyid")]
         public async Task<IActionResult> GetAnimePartById([FromQuery] int Id)
         {
-            var model = _dbContext.AnimeParts
-                .First(ap => ap.Id == Id);
+            var model = await _dbContext.AnimeParts.Include(ap=>ap.Anime)
+                .FirstOrDefaultAsync(ap => ap.Id == Id);
 
             if (model == null)
             {
                 return NotFound();
             }
-
-            return Ok(model);
+            var result = new
+            {
+                Id = model.Id,
+                AnimeTitle = model.Anime.Title,
+                SeasonNumber = model.SeasonNumber,
+                PartNumber = model.PartNumber,
+                Duration = model.Duration,
+                FileName = model.FileName,
+                FileUri = model.FileUri
+            };
+            return Ok(result);
         }
         [HttpGet("getbyseason")]
         public async Task<IActionResult> GetAnimePartsBySeason([FromQuery] int animeId, [FromQuery] int season)
         {
-            var model = _dbContext.AnimeParts.Where(ap => ap.AnimeId == animeId && ap.SeasonNumber == season)
+            var model = _dbContext.AnimeParts.Include(ap => ap.Anime).Where(ap => ap.AnimeId == animeId && ap.SeasonNumber == season)
                 .Select(a => new
                 {
                     Id = a.Id,
